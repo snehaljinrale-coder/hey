77700e65bf7fe5c8a7f865ce89e76bc3084060fd
snehaljinrale-coder <snehaljinrale@gmail.com>
2025-11-15 08:44:48 +0530

    Implement basic arithmetic with error handling
    Added error handling for invalid number inputs.

--- practice4.py
+++ practice4.py
@@ -1,1 +1,11 @@
+try:
+  a = float(input("Enter first number:"))
+  b = float(input("Enter second number:"))
+
+  print(f"Addition:{a + b}")
+  print(f"subtraction:{a - b}")
+except ValueError
+  print("Please enter valid numbers.")
+  
+  
 
